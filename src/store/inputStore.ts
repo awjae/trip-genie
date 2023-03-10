a3c1a699d1cc6a7666d69f47a3fe9d8d41fafc28
awjae <djwotmd@gmail.com>
2023-03-11 01:24:57 +0900

    [ADD] : 검색어  store 생성

---
+++ src/store/inputStore.ts
@@ -0,0 +1,9 @@
+import { InputForm } from '@/types/input';
+import { create } from 'zustand'
+
+const useDataStore = create((set) => ({
+  input: {},
+  setInput: ({ contry, destination, days }: InputForm) => set((state: any) => ({ InputForm: { contry, destination, days }})),
+}))
+
+export default useDataStore;
